8b6dd63dc669cf13740d206e5763039ce87e91d7
Divya <divya14402@gmail.com>
2022-04-29 13:19:46 +0530

    validation

--- src/Components/EditUser.jsx
+++ src/Components/EditUser.jsx
@@ -6,7 +6,12 @@ import {
   Button,
   makeStyles,
   Typography,
+  Box,
+  Dialog,
+  IconButton,
 } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import CloseIcon from '@material-ui/icons/Close';
 import { useEffect, useState } from 'react';
 import { editUser, getUsers } from '../Service/api';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -29,6 +34,10 @@ const initialValues = {
 };
 const EditUser = () => {
   const [user, setUser] = useState(initialValues);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState();
+  const [alertType, setAlertType] = useState();
+  const [userAlert, setUserAlert] = useState(false);
   const [formErrors, setFormErrors] = useState({});
   const { name, username, email, phone } = user;
   const { id } = useParams();
@@ -47,13 +56,26 @@ const EditUser = () => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
   const editUserDetails = async () => {
-    setFormErrors(validate(user));
     if (Object.keys(validate(user)).length === 0) {
-      setFormErrors(validate(user));
       await editUser(id, user);
       navigate('/');
+    } else {
+      setUserAlert(!userAlert);
     }
   };
+  const checkValidation = () => {
+    setFormErrors(validate(user));
+    if (Object.keys(validate(user)).length === 0) {
+      setAlertMessage('User verified successfully');
+      setAlertType('success');
+      setShowAlert(!showAlert);
+    } else {
+      setAlertMessage('Invalid user');
+      setAlertType('error');
+      setShowAlert(!showAlert);
+    }
+  };
+
   const validate = (values) => {
     const errors = {};
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
@@ -105,13 +127,41 @@ const EditUser = () => {
         <Input onChange={(e) => onValueChange(e)} name="phone" value={phone} />
       </FormControl>
       <p style={{ color: 'red' }}>{formErrors.phone}</p>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => editUserDetails()}
-      >
-        Edit User
-      </Button>
+      <Box display="flex" justifyContent="space-between">
+        <Button
+          variant="outlined"
+          color="primary"
+          style={{ width: '45%' }}
+          onClick={() => checkValidation()}
+        >
+          Validate
+        </Button>
+        <Dialog open={showAlert}>
+          <Alert severity={alertType}>
+            {alertMessage}
+            <IconButton aria-label="close" onClick={() => checkValidation()}>
+              <CloseIcon />
+            </IconButton>
+          </Alert>
+        </Dialog>
+        <Button
+          variant="contained"
+          color="primary"
+          size="medium"
+          style={{ width: '45%' }}
+          onClick={() => editUserDetails()}
+        >
+          Edit User
+        </Button>
+        <Dialog open={userAlert}>
+          <Alert severity="warning">
+            Please check the user details
+            <IconButton aria-label="close" onClick={() => editUserDetails()}>
+              <CloseIcon />
+            </IconButton>
+          </Alert>
+        </Dialog>
+      </Box>
     </FormGroup>
   );
 };
